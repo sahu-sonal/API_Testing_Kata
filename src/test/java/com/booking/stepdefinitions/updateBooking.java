44d6faed2ec68525cc5dc78d66b13660e7673fda
Daniele <danielemarinelli1387@gmail.com>
2025-06-26 20:05:20 +0200

    refactor update step def collecting bookingID int

--- src/test/java/com/booking/stepdefinitions/updateBooking.java
+++ src/test/java/com/booking/stepdefinitions/updateBooking.java
@@ -5,6 +5,7 @@ import com.support.SupportLibrary;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Assert;
 
@@ -19,19 +20,43 @@ public class updateBooking {
     }
     Response res;
 
-    @Given("I want to update a booking for a holiday")
-    public void user_has_access_to_endpoint() {
+    @Given("I have the bookingid for the roomid <{int}>")
+    public void iHaveTheBookingidForTheRoomid(int valueroomid) {
+
+        String tokenValue = context.getToken();
+        System.out.println("Token created __>> "+tokenValue);
+        res= given()
+                .queryParam("roomid",valueroomid)
+                .contentType(ContentType.JSON)
+                .cookie("token", tokenValue)
+                .get(URL.get_url_to_retrive_booking_room);
+        System.out.println(res.getBody().asString());
+        res.then()
+                .statusCode(200);
+
+        context.bookingID = res.jsonPath().get("bookings[0].bookingid");
 
     }
+
+
     @When("I make a request to update first name and Last name")
     public void user_makes_a_request_to_update_first_name_and_last_name() throws JsonProcessingException {
         String updateInfoClientJSONformat = "";
+        String tokenValue = context.getToken();
         updateInfoClientJSONformat = SupportLibrary.convertJavaObjectToJson(ClientInformation.updateClientNameAndLastName());
         res = given()
                 .log().all()
-                .pathParam("roomid",5)
+                .contentType(ContentType.JSON)
+                .accept("*/*")
+                .pathParam("bookingid",context.bookingID)
+                .cookie("token", tokenValue)
                 .body(updateInfoClientJSONformat)
-                .put(URL.put_url_to_update_booking_reservation);
+                .put(URL.put_url_to_update_booking_reservation+"/{bookingid}");
+                //.put("https://automationintesting.online/api/booking/4");
+        System.out.println(res.getBody().asString());
+
+        res.then()
+                .statusCode(200);
     }
     @Then("I should get the success status code")
     public void user_should_get_the_success_status_code() {
@@ -39,16 +64,21 @@ public class updateBooking {
         String tokenValue = context.getToken();
         System.out.println("Token created after update__>> "+tokenValue);
         res= given()
-                .queryParam("roomid",5)
+                .queryParam("roomid",1000)
+                .contentType(ContentType.JSON)
                 .cookie("token", tokenValue)
                 .get(URL.get_url_to_retrive_booking_room);
 
+        System.out.println(res.getBody().asString());
+        res.then()
+                .statusCode(200);
         String name = res.jsonPath().get("bookings[0].firstname").toString();
-        //Assert.assertEquals(name,"Jimmy");
+        Assert.assertEquals("Jimmy", name);
         System.out.println(name);
         String lastname = res.jsonPath().get("bookings[0].lastname").toString();
-        //Assert.assertEquals(lastname,"Love");
+        Assert.assertEquals("Love",lastname);
         System.out.println(lastname);
     }
+
 
 }

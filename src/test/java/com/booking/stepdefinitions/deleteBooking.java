cfdbf8256ee5dd8cde795a48fbfe2249f43e9a70
Daniele <danielemarinelli1387@gmail.com>
2025-06-26 20:03:03 +0200

    create stepdefinition for delete booking

--- src/test/java/com/booking/stepdefinitions/deleteBooking.java
+++ src/test/java/com/booking/stepdefinitions/deleteBooking.java
@@ -1,20 +1,35 @@
 package com.booking.stepdefinitions;
+import com.api.URL;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
 
 
 public class deleteBooking {
-    @Given("I want to delete my booking reservation")
-    public void i_want_to_delete_my_booking_reservation() {
 
+    TestContext context;
+
+    public deleteBooking(TestContext context){
+        this.context=context;
     }
+    Response res;
+
+
     @When("I make a request to delete it")
     public void i_make_a_request_to_delete_it() {
+        String tokenValue = context.getToken();
+        res = given()
+                .log().all()
+                .pathParam("bookingid",context.bookingID)
+                .cookie("token", tokenValue)
+                .delete(URL.delete_booking_reservation);
 
     }
     @Then("The reservation should be successfully deleted")
     public void the_reservation_should_be_successfully_deleted() {
-
+        res.then().statusCode(200);
     }
 }

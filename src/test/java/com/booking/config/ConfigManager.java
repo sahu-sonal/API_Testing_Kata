242a733cf34a63180755591cc8e268f4e159a6dc
sahu.sonal <sonalsahu91@gmail.com>
2025-11-23 20:17:17 +0530

    chore: Added application.properties and config manager files

---
+++ src/test/java/com/booking/config/ConfigManager.java
@@ -0,0 +1,57 @@
+package com.booking.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigManager {
+    private static final Properties properties = new Properties();
+    public static final String BASE_URL;
+    public static final String BOOKING_ENDPOINT;
+    public static final String AUTH_ENDPOINT;
+    public static final String USERNAME;
+    public static final String PASSWORD;
+
+    static {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("src/test/resources/application.properties");
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load application.properties file", e);
+        }
+    }
+
+    static {
+        // Initialize constants after properties are loaded
+        BASE_URL = getBaseUrl();
+        BOOKING_ENDPOINT = getBookingEndpoint();
+        AUTH_ENDPOINT = getAuthEndpoint();
+        USERNAME = getUsername();
+        PASSWORD = getPassword();
+    }
+
+    public static String getBaseUrl() {
+        return get("api.base.url");
+    }
+
+    public static String getBookingEndpoint() {
+        return BASE_URL + get("api.booking.endpoint");
+    }
+
+    public static String getAuthEndpoint() {
+        return BASE_URL + get("api.auth.endpoint");
+    }
+
+    public static String getUsername() {
+        return get("api.username");
+    }
+
+    public static String getPassword() {
+
+        return get("api.password");
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
+}
